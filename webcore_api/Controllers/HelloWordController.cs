2d7274280d316d84e14a9a9e3b008ab9f11bff72
Alex Kovalyov <alleexx6@gmail.com>
2021-04-07 13:05:59 +0300

    adding controller

---
+++ webcore_api/Controllers/HelloWordController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webcore_api.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class HelloWordController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult get()
+        {
+            return Ok("Hello Developer");
+        }
+    }
+}
